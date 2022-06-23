551033f443c307854bd949ec684fa8077591175e
Pavel Lantsman <pavel22lantsman@gmail.com>
2022-06-23 10:49:02 +0300

    Create Sprint 15 project

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const config = require('./common/config');
 const routes = require('./routes');
 
 
-const { DB_ADDRESS, PORT = 3000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -23,15 +23,11 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// mongoose.connect(DB_ADDRESS);
-
 mongoose.connect('mongodb+srv://PavelLan:Pavel22011978@cluster0.495ruax.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log("MongoDB connected."))
   .catch(error => console.log(error))
 
 app.use(routes);
-
-// app.listen(PORT);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
